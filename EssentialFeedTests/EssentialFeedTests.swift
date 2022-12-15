0697b7e27f4ace68714925e1083906dde7b94afc
Rafael Levy <srlevy2@gmail.com>
2022-12-14 17:19:29 -0800

    Guarantee not deliver result(closure) after RemoteFeedLoader Instance has been deallocated

--- EssentialFeedTests/EssentialFeedTests.swift
+++ EssentialFeedTests/EssentialFeedTests.swift
@@ -111,6 +111,26 @@ class EssentialFeedTests: XCTestCase {
         
     }
     
+    func test_load_doesNotDeliversResultAfterSUT_hasBeenDeallocated() {
+        let url = URL(string: "http://any-url.com")!
+        
+        let client = HTTPClientSpy()
+        
+        var sut: RemoteFeedLoader? = RemoteFeedLoader(url: url, client: client)
+        
+        var capturedResults = [RemoteFeedLoader.Result]()
+        sut?.load { capturedResults.append($0)}
+        
+        sut = nil
+        
+        client.complete(withStatusCode: 200, data: makeItemsJSON([]))
+        
+        XCTAssertTrue(capturedResults.isEmpty)
+        
+        
+        
+    }
+    
     
     
     
