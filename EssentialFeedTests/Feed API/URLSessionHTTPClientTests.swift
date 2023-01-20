acce4110401a805d72c40693222fe5a92c9d42e2
Rafael Levy <srlevy2@gmail.com>
2023-01-20 14:36:20 -0800

    Fixed thread race.Enable thread sanitizer on CI scheme

--- EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -203,7 +203,6 @@ class URLSessionHTTPClientTests: XCTestCase {
         
         
         override class func canInit(with request: URLRequest) -> Bool {
-            requestObserver?(request)
             return true
         }
         
@@ -212,7 +211,10 @@ class URLSessionHTTPClientTests: XCTestCase {
         }
         
         override func startLoading() {
-            
+            if let requestObserver = URLProtocolStub.requestObserver {
+                client?.urlProtocolDidFinishLoading(self)
+                return requestObserver(request)
+            }
             if let data = URLProtocolStub.stub?.data {
                 client?.urlProtocol(self, didLoad: data)
             }
@@ -226,6 +228,7 @@ class URLSessionHTTPClientTests: XCTestCase {
             }
             
             client?.urlProtocolDidFinishLoading(self)
+            
         }
         
         override func stopLoading() {
