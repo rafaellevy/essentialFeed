ef935e823113241788998bc76e646aa8df0e17a7
Rafael Levy <srlevy2@gmail.com>
2023-02-22 19:24:19 -0800

    Move Codable conformance from the framework-agnostic

--- EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -11,9 +11,31 @@ import EssentialFeed
  class CodableFeedStore {
      
      private struct Cache: Codable {
-              let feed: [LocalFeedImage]
-              let timestamp: Date
-          }
+         let feed: [CodableFeedImage]
+         let timestamp: Date
+         
+         var localFeed: [LocalFeedImage] {
+             return feed.map { $0.local }
+         }
+     }
+     
+     private struct CodableFeedImage: Codable {
+         private let id: UUID
+         private let description: String?
+         private let location: String?
+         private let url: URL
+         
+         init(_ image: LocalFeedImage) {
+             id = image.id
+             description = image.description
+             location = image.location
+             url = image.url
+         }
+         
+         var local: LocalFeedImage {
+             return LocalFeedImage(id: id, description: description, location: location, url: url)
+         }
+     }
 
           private let storeURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!.appendingPathComponent("image-feed.store")
      
@@ -24,12 +46,13 @@ import EssentialFeed
 
                   let decoder = JSONDecoder()
                   let cache = try! decoder.decode(Cache.self, from: data)
-                  completion(.found(feed: cache.feed, timestamp: cache.timestamp))
+                  completion(.found(feed: cache.localFeed, timestamp: cache.timestamp))
               }
 
               func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping FeedStore.InsertionCompletion) {
                   let encoder = JSONEncoder()
-                  let encoded = try! encoder.encode(Cache(feed: feed, timestamp: timestamp))
+                  let cache = Cache(feed: feed.map(CodableFeedImage.init), timestamp: timestamp)
+                  let encoded = try! encoder.encode(cache)
                   try! encoded.write(to: storeURL)
                   completion(nil)
      }
@@ -92,31 +115,6 @@ import EssentialFeed
               wait(for: [exp], timeout: 1.0)
           }
      
-     func test_retrieveAfterInsertingToEmptyCache_deliversInsertedValues() {
-              let sut = CodableFeedStore()
-              let feed = uniqueImageFeed().local
-              let timestamp = Date()
-              let exp = expectation(description: "Wait for cache retrieval")
-
-              sut.insert(feed, timestamp: timestamp) { insertionError in
-                  XCTAssertNil(insertionError, "Expected feed to be inserted successfully")
-
-                  sut.retrieve { retrieveResult in
-                      switch retrieveResult {
-                      case let .found(retrievedFeed, retrievedTimestamp):
-                          XCTAssertEqual(retrievedFeed, feed)
-                          XCTAssertEqual(retrievedTimestamp, timestamp)
-
-                      default:
-                          XCTFail("Expected found result with feed \(feed) and timestamp \(timestamp), got \(retrieveResult) instead")
-                      }
-
-                      exp.fulfill()
-                  }
-              }
-
-              wait(for: [exp], timeout: 1.0)
-          }
-
+     
 
  }
