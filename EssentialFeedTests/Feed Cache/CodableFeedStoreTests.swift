9273823362462e364434c4afd7d7da0d04a7f69b
Rafael Levy <srlevy2@gmail.com>
2023-02-22 20:24:54 -0800

    Add helper methods to provide documentation (context)

--- EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -71,13 +71,14 @@ import EssentialFeed
      override func setUp() {
               super.setUp()
 
-         try? FileManager.default.removeItem(at: testSpecificStoreURL())
+         setupEmptyStoreState()
+
           }
 
           override func tearDown() {
               super.tearDown()
 
-              try? FileManager.default.removeItem(at: testSpecificStoreURL())
+              undoStoreSideEffects()
           }
 
 
@@ -158,6 +159,18 @@ import EssentialFeed
      private func testSpecificStoreURL() -> URL {
               return FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first!.appendingPathComponent("\(type(of: self)).store")
           }
+     
+     private func setupEmptyStoreState() {
+         deleteStoreArtifacts()
+     }
+     
+     private func undoStoreSideEffects() {
+         deleteStoreArtifacts()
+     }
+     
+     private func deleteStoreArtifacts() {
+         try? FileManager.default.removeItem(at: testSpecificStoreURL())
+     }
      
 
  }
