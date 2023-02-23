1ee3e8e540890f6fe3ecc545c25b55f8d5d64bd3
Rafael Levy <srlevy2@gmail.com>
2023-02-22 20:07:43 -0800

    Extract system under test (sut) creation into a factory method

--- EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -76,7 +76,7 @@ import EssentialFeed
 
 
      func test_retrieve_deliversEmptyOnEmptyCache() {
-         let sut = CodableFeedStore()
+         let sut = makeSUT()
          let exp = expectation(description: "Wait for cache retrieval")
 
          sut.retrieve { result in
@@ -95,7 +95,7 @@ import EssentialFeed
      }
      
      func test_retrieve_hasNoSideEffectsOnEmptyCache() {
-              let sut = CodableFeedStore()
+              let sut = makeSUT()
               let exp = expectation(description: "Wait for cache retrieval")
 
               sut.retrieve { firstResult in
@@ -113,6 +113,38 @@ import EssentialFeed
               }
 
               wait(for: [exp], timeout: 1.0)
+          }
+     
+     func test_retrieveAfterInsertingToEmptyCache_deliversInsertedValues() {
+              let sut = makeSUT()
+              let feed = uniqueImageFeed().local
+              let timestamp = Date()
+              let exp = expectation(description: "Wait for cache retrieval")
+
+              sut.insert(feed, timestamp: timestamp) { insertionError in
+                  XCTAssertNil(insertionError, "Expected feed to be inserted successfully")
+
+                  sut.retrieve { retrieveResult in
+                      switch retrieveResult {
+                      case let .found(retrievedFeed, retrievedTimestamp):
+                          XCTAssertEqual(retrievedFeed, feed)
+                          XCTAssertEqual(retrievedTimestamp, timestamp)
+
+                      default:
+                          XCTFail("Expected found result with feed \(feed) and timestamp \(timestamp), got \(retrieveResult) instead")
+                      }
+
+                      exp.fulfill()
+                  }
+              }
+
+              wait(for: [exp], timeout: 1.0)
+          }
+     
+     // - MARK: Helpers
+          
+          private func makeSUT() -> CodableFeedStore {
+              return CodableFeedStore()
           }
      
      
