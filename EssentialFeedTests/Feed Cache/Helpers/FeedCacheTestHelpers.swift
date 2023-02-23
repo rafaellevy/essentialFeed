2b4f17f00c8c79af454912a6c05858b8e771ab48
Rafael Levy <srlevy2@gmail.com>
2023-02-22 18:16:19 -0800

    Extract cache policy from controller type

--- EssentialFeedTests/Feed Cache/Helpers/FeedCacheTestHelpers.swift
+++ EssentialFeedTests/Feed Cache/Helpers/FeedCacheTestHelpers.swift
@@ -31,10 +31,13 @@ extension Date {
         return adding(days: -feedCacheMaxAgeInDays)
          }
     
-    func adding(days: Int) -> Date {
+    private func adding(days: Int) -> Date {
         return Calendar(identifier: .gregorian).date(byAdding: .day, value: days, to: self)!
     }
     
+}
+
+extension Date {
     func adding(seconds: TimeInterval) -> Date {
         return self + seconds
     }
